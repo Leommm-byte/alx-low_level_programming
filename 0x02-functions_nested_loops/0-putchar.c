258a82488e42e8977074a41f0c8ae49f097ec7f6
Leommm-byte <leom20031@gmail.com>
2022-12-13 11:47:27 -0800

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char *a = "_putchar";
+	while (*a)
+	{
+		_putchar(*a);
+		a++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
