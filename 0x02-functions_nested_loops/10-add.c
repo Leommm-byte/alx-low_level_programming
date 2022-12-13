92d8d3f19ad32752e0694075a287f033c07bccbc
Leommm-byte <leom20031@gmail.com>
2022-12-13 15:17:51 -0800

    10. a + b

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,7 +1,8 @@
 #include "main.h"
 /**
  * add - a function that adds two integers and returns the result.
- *
+ * @one: first number input
+ * @two: second number input
  * Return: one + two
  */
 
