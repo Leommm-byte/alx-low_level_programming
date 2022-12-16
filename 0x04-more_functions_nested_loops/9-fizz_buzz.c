917172f8c06deb7fb88fb36db84e1f256b3986c3
Leommm-byte <leom20031@gmail.com>
2022-12-15 16:38:41 -0800

    9. Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,7 +2,9 @@
 
 /**
  * main - “Fizz-Buzz test”
- *
+ * 3 multiples print Fizz instead of the number
+ * 5 multiples print Buzz instead of the number
+ * 3 & 5 multiples print FizzBuzz instead of the number
  * Return: Always 0 (Success)
  */
 
